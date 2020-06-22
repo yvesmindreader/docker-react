00d39eac0c1ae176bdcbdc661a29b9bd804f435a
Yafeng Zhang <yvesmindreader@outlook.com>
2020-06-23 02:26:29 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Yves! You can be great!
         </p>
         <a
           className="App-link"
